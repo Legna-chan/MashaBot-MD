a0ff05371b2fd680b3ed2f4464c8bbedeadaaf8b
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-23 22:45:06 -0700

    Actualizar config.js

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ global.packsticker2 = `━━━━━━━━♾\n⪛✰ 𝐘𝐮𝐤𝐢-𝐒
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = '⏤͟͟͞͞🐻𝐌𝐀𝐒𝐇𝐀ᵇᵒᵗ ᥫ᭡'
+global.packname = '⏤͟͟͞͞🧡𝐌𝐀𝐒𝐇𝐀ᵇᵒᵗ ᥫ᭡'
 global.botname = 'Maria Mikhailovna Kujou'
 global.wm = 'Masha Kujou'
 global.author = 'Made By Legna'
