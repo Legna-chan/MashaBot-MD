54e37ea02196a7a5275eca589bb92cc2ae101333
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-24 00:11:20 -0700

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -13,11 +13,11 @@ let handler = async (m, { conn, args }) => {
 
   let txt = ` 𝙷𝚘𝚕𝚊! 𝚋𝚒𝚎𝚗𝚟𝚎𝚗𝚒𝚍@ 𝚊𝚕 𝚖𝚎𝚗𝚞 𝚍𝚎 *${botname}*
 ╭─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒
-│🤍 Cliente » @${userId.split('@')[0]}
-│ ✰ ${moneda} » ${coins}
-│ ꕥ Experiencia » ${exp.toLocaleString()}
-│ ✤ Nivel » ${nivel}
-│ 🜲 Rango » ${role}
+│✿ Cliente » @${userId.split('@')[0]}
+│✰ ${moneda} » ${coins}
+│ꕥ Experiencia » ${exp.toLocaleString()}
+│✤ Nivel » ${nivel}
+│🜲 Rango » ${role}
 │${dev}
 ╰─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒
 
