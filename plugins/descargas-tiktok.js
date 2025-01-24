22c22b09a7aed75cf9e646102a86bd9718bb1714
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-23 23:17:26 -0700

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch';
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) {
-        return conn.reply(m.chat, '🍬 Por favor, ingresa un enlace de TikTok.', m);
+        return conn.reply(m.chat, '✿ Por favor, ingresa un enlace de TikTok.', m);
     }
 
     try {
-        await conn.reply(m.chat, "🍭 Espere un momento, estoy descargando su video...", m);
+        await conn.reply(m.chat, "✿ Espere un momento, estoy descargando su video...", m);
 
         const tiktokData = await tiktokdl(args[0]);
 
