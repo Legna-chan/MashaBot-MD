cfd5023c0a023c7d0fbafb2a301f6ee60777f670
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-26 00:58:59 -0700

    Actualizar _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -8,13 +8,13 @@ let usuario = `@${m.sender.split`@`[0]}`
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'  
 
 let nombre, foto, edit, newlink, status, admingp, noadmingp
-nombre = `*${usuario}*\n🌻 Ha cambiado el nombre del grupo.\n\n🐻 Ahora el grupo se llama:\n*<${m.messageStubParameters[0]}>*...`
-foto = `*${usuario}*\n🌻 Ha cambiado la imagen del grupo...`
-edit = `*${usuario}*\n🐻 Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo...`
-newlink = `🌻 El enlace del grupo ha sido restablecido por:\n*» ${usuario}*...`
-status = `🌻 El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n🐻 Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje...`
-admingp = `*@${m.messageStubParameters[0].split`@`[0]}* Ahora es admin del grupo 🐻\n\n🌻 Acción hecha por:\n*» ${usuario}*...`
-noadmingp =  `*@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo 🐻\n\n🌻 Acción hecha por:\n*» ${usuario}*...`
+nombre = `*${usuario}*\n🍡 Ha cambiado el nombre del grupo.\n\n🪷 Ahora el grupo se llama:\n*<${m.messageStubParameters[0]}>*...`
+foto = `*${usuario}*\n🍡 Ha cambiado la imagen del grupo...`
+edit = `*${usuario}*\n🪷 Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo...`
+newlink = `🍡 El enlace del grupo ha sido restablecido por:\n*» ${usuario}*...`
+status = `🍡 El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n🪷 Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje...`
+admingp = `*@${m.messageStubParameters[0].split`@`[0]}* Ahora es admin del grupo 🦄\n\n🍡 Acción hecha por:\n*» ${usuario}*...`
+noadmingp =  `*@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo 🦄\n\n🍡 Acción hecha por:\n*» ${usuario}*...`
 
 if (chat.detect && m.messageStubType == 21) {
 await conn.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: fkontak })   
