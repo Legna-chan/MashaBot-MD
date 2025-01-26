1a21ff705b068da68d2a8c71b39fe8cb44d18a10
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-26 02:19:46 -0700

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -58,7 +58,7 @@ if (isNewLogin) {
 conn.isInit = true
 }
 if (qr) {
-let txt = '🌸 S E R B O T ➩ B O T 🌸 \n\n*Escanea este QR para ser un Sub Bot*\n\nPasos para escanear:\n\n`1` : Haga click en los 3 puntos\n\n`2` : Toque dispositivos vinculados\n\n`3` : Escanea este QR\n\n> *Nota:* Este código QR expira en 30 segundos.'
+let txt = '🌸 S E R B O T • S U B 🌸 \n\n*Escanea este QR para ser un Sub Bot*\n\nPasos para escanear:\n\n`1` : Haga click en los 3 puntos\n\n`2` : Toque dispositivos vinculados\n\n`3` : Escanea este QR\n\n> *Nota:* Este código QR expira en 30 segundos.'
 
 let sendQR = await parentw.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), "qrcode.png", txt, m, null, rcanal)
 setTimeout(() => {
