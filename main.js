d209c40d02462501e4adf789acdc1dd450835189
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-26 08:16:11 -0700

    Actualizar main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import fs, {readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileSy
 import yargs from 'yargs';
 import {spawn} from 'child_process'
 import lodash from 'lodash'
-import { SagiriJadiBot } from './plugins/jadibot-serbot.js';
+import { YukiJadiBot } from './plugins/jadibot-serbot.js';
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import {tmpdir} from 'os'
