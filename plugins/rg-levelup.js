7c85c5f1d9d17efa5cd92d7f96cc640917a5d0c5
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-26 20:33:36 -0700

    Actualizar rg-levelup.js

--- plugins/rg-levelup.js
+++ plugins/rg-levelup.js
@@ -20,7 +20,7 @@ txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
 txt += `• ✿ Nivel anterior : ${before}\n`
 txt += `• ✿ Nuevos niveles : ${user.level}\n`
 txt += `• 📆 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
-txt += `> Cuanto más a interactúes con sagiri,mayor será tu nivel.`
+txt += `> Cuanto más a interactúes con sagiri, mayor será tu nivel.`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, m)}}
 
 handler.help = ['levelup']
