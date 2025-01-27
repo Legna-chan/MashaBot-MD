2b994fb53c7ce0c3aef070502a9726290303e2ce
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-26 18:07:52 -0700

    Actualizar rpg-cf.js

--- plugins/rpg-cf.js
+++ plugins/rpg-cf.js
@@ -3,28 +3,28 @@ let users = {};
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [eleccion, cantidad] = text.split(' ');
     if (!eleccion || !cantidad) {
-        return m.reply(`🍬 Por favor, elige cara o cruz y una cantidad de ${moneda} para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`🍡 Por favor, elige cara o cruz y una cantidad de ${moneda} para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     eleccion = eleccion.toLowerCase();
     cantidad = parseInt(cantidad);
     if (eleccion !== 'cara' && eleccion !== 'cruz') {
-        return m.reply(`🍭 Elección no válida. Por favor, elige cara o cruz.\nEjemplo: *${usedPrefix + command} cara*`);
+        return m.reply(`🪷 Elección no válida. Por favor, elige cara o cruz.\nEjemplo: *${usedPrefix + command} cara*`);
     }
 
     if (isNaN(cantidad) || cantidad <= 0) {
-        return m.reply(`🍭 Cantidad no válida. Por favor, elige una cantidad de ${moneda} para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`🍡 Cantidad no válida. Por favor, elige una cantidad de ${moneda} para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     let userId = m.sender;
     if (!users[userId]) users[userId] = { coin: 100 };
     let user = global.db.data.users[m.sender];
     if (user.coin < cantidad) {
-        return m.reply(`🍭 No tienes suficientes ${moneda} para apostar. Tienes ${user.coin} ${moneda}.`);
+        return m.reply(`🪷 No tienes suficientes ${moneda} para apostar. Tienes ${user.coin} ${moneda}.`);
     }
 
     let resultado = Math.random() < 0.5 ? 'cara' : 'cruz';
-   let mensaje = `🍬 La moneda ha caído en `
+   let mensaje = `🍡 La moneda ha caído en `
     if (resultado === eleccion) {
         user.coin += cantidad; 
     mensaje += `*${resultado}* y has ganado *${cantidad} ${moneda}*!`;
