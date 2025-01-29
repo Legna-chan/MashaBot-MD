fb9b21d07acc86309210a16346d0b669743d5b96
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-28 22:09:40 -0700

    Actualizar rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -23,7 +23,7 @@ fechaBio = biografia[0].setAt ? new Date(biografia[0].setAt).toLocaleDateString(
   let pp = await conn.profilePictureUrl(who, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🪷 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
+  if (user.registered === true) return m.reply(`🪷 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\n> Use este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
   if (!Reg.test(text)) return m.reply(`🍡 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) return m.reply('🍡 El nombre no puede estar vacío.')
