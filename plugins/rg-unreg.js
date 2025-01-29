51c4fcb536fa82234d6a5944c9d89d0b18b5f67f
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-28 22:05:17 -0700

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*🍡 Ya no estas en mi base de datos :3*`, m, m)
+return conn.reply(m.chat, `✿ Ya no estas en mi base de datos :3`, m, m)
 
 }
 handler.help = ['unreg']
