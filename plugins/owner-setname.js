0424ed9a5d4e6c46c3d2b9fad880f14c5b0c536e
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-31 15:37:38 -0700

    Actualizar owner-setname.js

--- plugins/owner-setname.js
+++ plugins/owner-setname.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply('🍬 Por favor, proporciona un nombre para el bot.\n> Ejemplo: #setname Nombre/Texto');
+  if (!text) return m.reply('🍡 Por favor, proporciona un nombre para el bot.\n> Ejemplo: #setname Nombre/Texto');
 
   const names = text.split('/');
-  if (names.length !== 2) return m.reply('🍬 Por favor, proporciona ambos nombres separados por una barra (/) en el formato: nombre1/nombre2.');
+  if (names.length !== 2) return m.reply('🪷 Por favor, proporciona ambos nombres separados por una barra (/) en el formato: nombre1/nombre2.');
 
   global.botname = names[0].trim();
-  const texto1bot = ', Powered By 🌻┋ŀ𝔢ᶢᶰᾰ ᥫ᭡፝֟፝֟';
+  const texto1bot = ', Powered By Legna;
   global.textbot = `${names[1].trim()}${texto1bot}`;
   
-  m.reply(`🍬 El nombre del bot ha sido cambiado a: ${global.botname}\n\n🍭 El texto del bot ha sido cambiado a: ${global.textbot}`);
+  m.reply(`🍡 El nombre de la bot ha sido cambiado a: ${global.botname}\n\n🪷 El texto del bot ha sido cambiado a: ${global.textbot}`);
 };
 
 handler.help = ['setname'];
