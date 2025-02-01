0a4a8762965dcb99db66f82c56cc200610f3af7b
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-01-31 20:31:32 -0700

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,19 +7,19 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = ` 「🍭」SatouBot - MD \n「 Bienvenido :3 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bienvenido/a 」\n「 ${groupMetadata.subject} 」\n\n> ✐ Puedes usar *.menu* para ver la lista de comandos.
+    let welcome = ` 「✿」Sagiri - MD \n「 Bienvenido :3 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bienvenido/a 」\n「 ${groupMetadata.subject} 」\n\n> ✐ Usa *#ayuda* para ver mi menu.
 > 🜸 https://github.com/Legna-chan`
 await conn.sendMini(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = ` 「🍭」SatouBot - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Me podrias dar una estrellita en el repositorio oficial de la bot?
+    let bye = ` 「✿」Sagiri - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Usa *#ayuda* para ver menu.
 > 🜸 https://github.com/Legna-chan`
 await conn.sendMini(m.chat, packname, textbot, bye, img, img, redes, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `「🍭」SatouBot - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3」\n\n> ✐ Me podrias dar una estrellita en el repositorio oficial de la bot?
+    let kick = `「✿」Sagiri - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3」\n\n> ✐ Usa *#ayuda* para ver mi menu. 
 > 🜸 https://github.com/Legna-chan`  
 await conn.sendMini(m.chat, packname, textbot, kick, img, img, redes, fkontak)
 await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
